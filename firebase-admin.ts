62bc8f4e52093be9d09c8d102e0f6858fc8b6387
dheemanthm04 <dheemanth.cs22@bmsce.ac.in>
2025-05-04 22:24:17 +0530

    Refactor: Load Firebase credentials from environment variable

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,12 +1,11 @@
 import { initializeApp, cert, getApps, getApp, App } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-// Use require OR import depending on your setup
-const serviceKey = require("@/service_key.json");
-
 let app: App;
 
 if (getApps().length === 0) {
+  const serviceKey = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON!);
+
   app = initializeApp({
     credential: cert(serviceKey),
   });
